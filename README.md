26a151f4299fea1c914b2cbe1d54940a55d36e80
Anthologia <pyoungjy@gmail.com>
2022-02-14 01:08:14 +0900

    스프링 웹 개발 기초 - API

--- README.md
+++ README.md
@@ -12,3 +12,9 @@
 ### 2) MVC와 템플릿 엔진
 - @RequestParam을 사용한 Controller 구성
 - Controller와 viewResolver, Template engine 간의 기본적인 상호작용 살펴보기
+### 3) API
+- @ResponseBody를 사용한 Controller 구성
+- @ResponseBody를 사용한 Controller와 HttpMessageConveter 간의 기본적인 상호작용 살펴보기
+  - 객체를 반환할 경우, MappingJackson2HttpMessageConveter가 호출되어 사용된다.  
+    이때, 객체의 멤버변수 접근지정자가 private라면 getter가 있어야만 노출된다.
+
--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -2,8 +2,7 @@ package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
@@ -18,5 +17,33 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // 객체를 json으로 반환한다. 단, 접근지정자가 private인 경우 getter가 있어야만 노출된다.
+    }
+
+    static class Hello {
+        private String name;
+        private String test = "test";
+        public int pw = 1234;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
